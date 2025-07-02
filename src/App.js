fc15a89cce59ffe0bcd236d40f2eabf3303ae63e
victoriaag6 <victoriaacostag6@gmail.com>
2025-07-02 13:54:58 -0400

    hola

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+      <h1>¡Proyecto SST Front funcionando!</h1>
+      <p>Bienvenido a tu proyecto base con React + Vite.</p>
+    </div>
+  );
+}
+
+export default App;
